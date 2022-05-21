66bad69098eb9ba378557701c16d7e48e8109cf9
Inoi-K <greatoat@gmail.com>
2022-05-21 13:47:51 +0300

    Double Buffer on Actors

---
+++ Assets/Scripts/Double Buffer/Comedian.cs
@@ -0,0 +1,26 @@
+﻿public class Comedian : TheaterActor {
+    TheaterActor facingActor;
+
+    string slappedLine;
+    string idleLine;
+
+    void OnEnable() {
+        slappedLine = $"{nickname} was slapped, so he slaps {facingActor.nickname}";
+        idleLine = $"{nickname} was not slapped, so he does nothing";
+    }
+
+    public void Face(TheaterActor actor) {
+        facingActor = actor;
+    }
+    
+    public override void React() {
+        if (currentSlapped)
+            facingActor.GetSlapped();
+        
+        PrintInfo(currentSlapped);
+    }
+
+    void PrintInfo(bool isSlapped) {
+        print(isSlapped ? slappedLine : idleLine);
+    }
+}
